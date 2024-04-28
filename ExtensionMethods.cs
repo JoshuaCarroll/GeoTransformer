f015a73b3b9293b5ec900178a94e79389c28579b
Joshua Carroll <joshcarroll21@gmail.com>
2024-04-27 23:06:45 -0500

    Created extension method to test for null enumerables

---
+++ ExtensionMethods.cs
@@ -0,0 +1,10 @@
+namespace ExtensionMethods
+{
+    public static class MyExtensions
+    {
+        public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> source)
+        {
+            return source ?? Enumerable.Empty<T>();
+        }
+    }
+}
